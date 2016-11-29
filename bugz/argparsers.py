2450e255482cd3718ed3faa28120026a7c242005
Zoya Laletina <z.laletina@ispsystem.com>
2016-11-29 11:40:49 +0800

    Revert "show-core-branch"
    This reverts commit 0992e04cb4bc749d34d80e6935ddc0580eaa072b.

--- bugz/argparsers.py
+++ bugz/argparsers.py
@@ -220,8 +220,6 @@ def make_search_parser(subparsers):
 		help = 'email the bug is assigned to')
 	search_parser.add_argument('--cf_branch',
 		help = 'branch in which the bug is resolved')
-	search_parser.add_argument('--cf_core_branch',
-		help = 'core-branch in which the bug is resolved')
 	search_parser.add_argument('--cf_autotest',
 		help = 'cf_autest params')
 	search_parser.add_argument('--qa-contact',
@@ -268,9 +266,6 @@ def make_search_parser(subparsers):
 	search_parser.add_argument('--show-branch',
 		action = 'store_true',
 		help='show branch of bugs')
-	search_parser.add_argument('--show-core-branch',
-		action = 'store_true',
-		help='show core branch of bugs')
 	search_parser.add_argument('--show-teststatus',
 		action = 'store_true',
 		help='show autotest status of bugs')
